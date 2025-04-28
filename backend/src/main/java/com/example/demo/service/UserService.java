4fd36471e0d77a5268d739604143eba3ddb59ec9
AnjanaTharanga24 <it21826436@my.sliit.lk>
2025-04-28 21:52:59 +0530

    created login function

--- backend/src/main/java/com/example/demo/service/UserService.java
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.request.LoginRequest;
 import com.example.demo.dto.request.UserRequest;
 import com.example.demo.dto.response.UserResponse;
 import com.example.demo.exception.NotFoundException;
@@ -7,5 +8,5 @@ import com.example.demo.exception.NotFoundException;
 public interface UserService {
 
     UserResponse register(UserRequest userRequest)throws NotFoundException;
-    UserResponse login(UserRequest userRequest)throws NotFoundException;
+    UserResponse login(LoginRequest loginRequest)throws NotFoundException;
 }
